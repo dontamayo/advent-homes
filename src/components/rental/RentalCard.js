1ec637edf5603e1980ddf114d77218dd42dc1d38
Don aldrich Tamayo <amazingmardon@gmail.com>
2020-05-27 00:52:09 -0400

    styling rental categories

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -5,7 +5,7 @@ const RentalCard = ({rental}) => {
         <div className="card bwm-card">
           <img className="card-img-top" src={rental.image} alt={rental.title} />
           <div className="card-body">
-            <h6 className="card-subtitle mb-0 text-muted">
+            <h6 className={`card-subtitle mb-0 type-${rental.category}`}>
               {rental.shared ? "Shared " : "Whole "}
               {rental.category} &#183; {rental.city}
             </h6>
