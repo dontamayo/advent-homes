36f0fed1880bf621617ad2f9b86efe04c671dac4
Don aldrich Tamayo <amazingmardon@gmail.com>
2020-05-26 23:40:32 -0400

    rental homes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/shared/Header';
+import RentalHome from './pages/RentalHome';
 
 function App(){
     return(
     <div>
          <Header/>
+         <RentalHome/>
     </div>
     )
   }
