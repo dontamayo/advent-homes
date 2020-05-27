0b589fc0177c46bc4ac8b5729008de5da2cd2981
Don aldrich Tamayo <amazingmardon@gmail.com>
2020-05-26 23:08:43 -0400

    added scss

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+/* eslint-disable jsx-a11y/anchor-is-valid */ 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="#">
-        Navbar
+        AdventHomes
       </a>
       <button
         className="navbar-toggler"
@@ -19,15 +20,21 @@ const Header = () => {
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
+        <form className="form-inline my-2 my-lg-0">
+            <input
+            className="form-control mr-sm-2 ah-search"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            />
+            <button className="btn btn-ah-main btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+            </button>
+        </form>
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
             <a className="nav-link" href="#">
               Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
             </a>
           </li>
           <li className="nav-item dropdown">
@@ -40,7 +47,7 @@ const Header = () => {
               aria-haspopup="true"
               aria-expanded="false"
             >
-              Dropdown
+              Manage
             </a>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
               <a className="dropdown-item" href="#">
@@ -56,22 +63,16 @@ const Header = () => {
             </div>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">
-              Disabled
+            <a className="nav-link" href="#">
+              Login
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Register
             </a>
           </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-            Search
-          </button>
-        </form>
       </div>
     </nav>
   );
